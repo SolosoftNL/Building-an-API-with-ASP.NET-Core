2cbf0c4a30d673efe64322db1d52558d151925da
RocketRon <solosoft_nl@gmail.com>
2020-10-29 16:58:51 +0100

    removed old code

--- Data/CampProfile.cs
+++ Data/CampProfile.cs
@@ -21,11 +21,7 @@ namespace CoreCodeCamp.Data
 				.ForMember(c => c.CityTown, o => o.MapFrom(m => m.Location.CityTown))
 				.ForMember(c => c.StateProvince, o => o.MapFrom(m => m.Location.StateProvince))
 				.ForMember(c => c.PostalCode, o => o.MapFrom(m => m.Location.PostalCode))
-				.ForMember(c => c.Country, o => o.MapFrom(m => m.Location.Country))
-				//.ForMember(c=>c.Talks, o=>o.MapFrom(m=>m.Talks))
-			
-				;
-        //   this.CreateMap<Camp, TalkModel>().ForMember(c=> c.Abstract, o=> o.ConvertUsing<CampModel>()									
+				.ForMember(c => c.Country, o => o.MapFrom(m => m.Location.Country));
 		}
 	}
 }
