b97aeaa08d3d560ec703e19d00413d82500ae2cf
RocketRon <solosoft_nl@gmail.com>
2020-10-29 16:54:40 +0100

    Add project files.

---
+++ Controllers/CampsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CampsController : ControllerBase
+	{
+		private readonly ICampRepository _repository;
+		private readonly IMapper _mapper;
+
+		public CampsController(ICampRepository repository, IMapper mapper)
+		{
+			this._repository = repository;
+			this._mapper = mapper;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAllCamps(bool includeTalks = false)
+		{
+			try
+			{
+				var results = await _repository.GetAllCampsAsync(includeTalks: includeTalks);
+
+				CampModel[] models = _mapper.Map<CampModel[]>(results);
+
+				return this.Ok(models);
+
+			}
+			catch (Exception ex)
+			{
+
+				return this.StatusCode(StatusCodes.Status500InternalServerError, ex.Message.ToString());
+			}
+		}
+
+		//nieuwe wijze van implementatie
+		[HttpGet("{moniker}")]
+		public async Task<ActionResult<CampModel>> GetCamp(string moniker)
+		{
+			try
+			{
+				var result = await _repository.GetCampAsync(moniker);
+
+				if (result == null) return NotFound();
+
+				return _mapper.Map<CampModel>(result);
+			}
+			catch (Exception)
+			{
+				return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+			}
+
+
+		}
+
+
+
+	}
+}
---
+++ Data/CampProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Data
+{
+	public class CampProfile : Profile
+	{
+		public CampProfile()
+		{
+			this.CreateMap<Speaker, SpeakerModel>();
+			this.CreateMap<Talk, TalkModel>();
+			this.CreateMap<Camp, CampModel>()
+				.ForMember(c => c.Venue, o => o.MapFrom(m => m.Location))
+				.ForMember(c => c.Address1, o => o.MapFrom(m => m.Location.Address1))
+				.ForMember(c => c.Address2, o => o.MapFrom(m => m.Location.Address2))
+				.ForMember(c => c.Address3, o => o.MapFrom(m => m.Location.Address3))
+				.ForMember(c => c.CityTown, o => o.MapFrom(m => m.Location.CityTown))
+				.ForMember(c => c.StateProvince, o => o.MapFrom(m => m.Location.StateProvince))
+				.ForMember(c => c.PostalCode, o => o.MapFrom(m => m.Location.PostalCode))
+				.ForMember(c => c.Country, o => o.MapFrom(m => m.Location.Country))
+				//.ForMember(c=>c.Talks, o=>o.MapFrom(m=>m.Talks))
+			
+				;
+        //   this.CreateMap<Camp, TalkModel>().ForMember(c=> c.Abstract, o=> o.ConvertUsing<CampModel>()									
+		}
+	}
+}
